f3c0bc01336ca0aaeddf06a5c8cc1bd55bb4f87d
huy <huyn102010@gmail.com>
2021-10-14 22:08:18 -0400

    Finished the function

--- 99_bottles.py
+++ 99_bottles.py
@@ -1,7 +1,20 @@
-def bottle_song():
+def bottle_song(num_bottle):
 	# write your code here!
+	verse ="""
+	{0} {2} of beer on the wall
+	{0} {2} of beer
+	Take one down, pass it around
+	{1} {3} of beer on the wall
+	"""
+	for i in range(num_bottle,0,-1):
+		print(verse.format(
+		i, #0
+		i-1, #1
+		"bottle" if i == 1 else "bottles", #2
+		"bottle" if i-1 == 1 else "bottles"))#3
+	
+	
 
-
-bottle_song()
+bottle_song(99)
 
 ## Hello world ! 
