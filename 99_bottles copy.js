1640a5ae36b24b2438b50a4ff56c5fda4858a38e
eitanzziv <eitanzziv@penguin.lxd>
2022-03-10 07:00:29 -0500

    implemented bottleSong function

---
+++ 99_bottles copy.js
@@ -0,0 +1,5 @@
+function bottleSong() {
+ let numBot=99
+ for (let i=0;i<numBot;i++){console.log(`$(numBot) bottles of beer on the wall, $(numBot) bottles of beer.  Take one down, pass it around, $(numBot-1)bottles of beer on the wall.`)}
+};
+bottleSong();
