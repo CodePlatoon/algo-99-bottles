3b2075b99f8ba80de91b49e801790dd4de40bafb
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-03-11 21:21:09 -0500

    Completed 99 bottles program in Python

--- 99_bottles.py
+++ 99_bottles.py
@@ -1,5 +1,13 @@
 def bottle_song():
-	# write your code here!
-
+	bottles = 99
+	while bottles > 0: 
+		if bottles > 1:
+			print(bottles,"bottles of beer on the wall,",bottles,"bottles of beer.")
+			bottles -= 1
+			print("Take one down and pass it around,",bottles,"bottles of beer on the wall.")
+		else: 
+			print(bottles,"bottle of beer on the wall,",bottles,"bottle of beer.")
+			bottles -= 1
+			print("Take one down and pass it around,",bottles,"bottles of beer on the wall.")
 
 bottle_song()
