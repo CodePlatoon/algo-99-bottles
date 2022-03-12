1e2015054d3f29778a4be5b68df707009b4b61d0
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-03-11 20:50:13 -0500

    Created 99 bottles of beer program using while loop.

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,5 +1,16 @@
 function bottleSong() {
-  // Write your code here!
+  var bottles = 99;
+  while (bottles > 0) {
+    if (bottles > 1){
+      console.log(bottles + " bottles of beer on the wall, " + bottles + " bottles of beer.")
+      bottles -= 1;
+      console.log("Take one down and pass it around, " + bottles + " bottles of beer on the wall.")
+    } else {
+      console.log(bottles + " bottle of beer on the wall, " + bottles + " bottle of beer.")
+      bottles -= 1;
+      console.log("Take one down and pass it around, " + bottles + " bottles of beer on the wall.")
+    } 
+  }
 };
 
 bottleSong();
