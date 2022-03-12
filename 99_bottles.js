a1e0e0fa1d26a5c81123a378fee8f3e59114b98c
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-03-12 09:22:09 -0500

    Updated end of 99 bottles song

--- 99_bottles.js
+++ 99_bottles.js
@@ -8,7 +8,9 @@ function bottleSong() {
     } else {
       console.log(bottles + " bottle of beer on the wall, " + bottles + " bottle of beer.")
       bottles -= 1;
-      console.log("Take one down and pass it around, " + bottles + " bottles of beer on the wall.")
+      console.log("Take one down and pass it around, no more bottles of beer on the wall.")
+			console.log("No more bottles of beer on the wall, no more bottles of beer.")
+			console.log("Go to the store and buy some more, 99 bottles of beer on the wall.")
     } 
   }
 };
