90a9deb9ab2274bc144100f9617d855f2b514a5d
grizzdank <dave.e.graham@gmail.com>
2024-03-02 11:28:50 -0800

    Final changes

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,5 +1,23 @@
 function bottleSong() {
   // Write your code here!
+  let cnt = 99
+  while (cnt >= 0) {
+    if (cnt >1) {
+    console.log(cnt + " bottles of beer on the wall, " + cnt + " bottles of beer");
+    cnt--;
+    console.log("Take one down, pass it around, " +cnt+ " bottles of beer on the wall.");
+    }
+    else if (cnt === 1) {
+    console.log(cnt + " bottle of beer on the wall, " +cnt+ " bottle of beer.");
+    cnt--;
+    console.log("Take one down pass it around, no more bottles of beer on the wall.");
+    }
+    else {
+      console.log("No more bottles of beer on the wall, no more bottles of beer.");
+      console.log("Go to the store and buy some more, 99 bottles of beer on the wall");
+      cnt--;
+    }
+  }
 };
 
 bottleSong();
--- 99_bottles.py
+++ 99_bottles.py
@@ -4,7 +4,7 @@
 
 
 def bottle_song():
-	cnt = int(input('How many bottles would you like to sing about? '))
+    cnt = int(input('How many bottles would you like to sing about? '))
     while cnt > 1:
         message = f'{cnt} bottles of beer on the wall, {cnt} bottles of beer.'
         print(message)
